f81cc6aac9427c6e8f7daf2cd0168b589d2d59d0
Nithur <nithur97@gmail.com>
2022-06-26 16:54:04 +0530

    link card added

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,13 @@ export default function Index() {
       <Head>
         <title>Highlightify: Text highlight on image</title>
         <meta name="description" content="A simple text highlighter for images. Highlight text on images before sharing on social media. " />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+        <meta property="twitter:url" content="highlightify.vercel.app" />
+        <meta property="twitter:title" content="Highlight Text in Images" />
+        <meta property="twitter:description" content="A simple text highlighter for images. Highlight text on images before sharing on Twitter. " />
+        <meta property="twitter:image" content="https://github.com/Nithur-M/PubImages/blob/main/Highlight%20Text%20in%20Images.png"/>
       </Head>
 
       <Flex>
